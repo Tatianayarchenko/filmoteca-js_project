81a01c68d5a3635beb2cebeff0816cc55aaff0e8
Tatianayarchenko <ktanya61@gmail.com>
2022-06-07 19:45:44 +0300

    закінчила хедер

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 const refs = {
   header: document.querySelector('#header'),
   //   logo: document.querySelector('#header-logo'),
+  //   homeButton: document.querySelector('#home_btn'),
+  //   libraryButton: document.querySelector('#library_btn'),
   homeButton: document.querySelector('#home_btn'),
   libraryButton: document.querySelector('#library_btn'),
   homeCurrentPage: document.querySelector('#home-current-page'),
   libraryCurrentPage: document.querySelector('#library-current-page'),
   searchForm: document.querySelector('#search-form'),
-  heroLibraryContainer: document.querySelector('.hero-library-container'),
+  //   heroLibraryContainer: document.querySelector('.hero-library-container'),
+  hederLibraryPage: document.querySelector('#header-library'),
+  //   formButton: document.querySelector('#form-button'),
+  //   libraryWatchedButton: document.querySelector('#library-watched__button'),
+  //   libraryQueueButton: document.querySelector('#library-queue__button'),
 };
 
 refs.homeButton.addEventListener('click', onHomePage);
@@ -15,8 +21,7 @@ refs.libraryButton.addEventListener('click', onLibraryPage);
 function onHomePage() {
   console.log('клик по кнопке HOME');
   refs.searchForm.classList.remove('is-hidden');
-  //   refs.searchForm.classList.add('search-form');
-  refs.heroLibraryContainer.classList.add('is-hidden');
+  refs.hederLibraryPage.classList.add('is-hidden');
   refs.header.classList.remove('library');
   refs.header.classList.add('home');
   refs.homeCurrentPage.classList.add('current-page');
@@ -25,12 +30,11 @@ function onHomePage() {
 
 function onLibraryPage() {
   console.log('клик по кнопке Library');
-
   refs.header.classList.remove('home');
   refs.header.classList.add('library');
-  refs.heroLibraryContainer.classList.remove('is-hidden');
+  refs.hederLibraryPage.classList.remove('is-hidden');
   refs.searchForm.classList.add('is-hidden');
-  refs.heroLibraryContainer.classList.add('hero-library-container');
+  refs.hederLibraryPage.classList.add('hero-library-container');
   refs.homeCurrentPage.classList.remove('current-page');
   refs.libraryCurrentPage.classList.add('current-page');
 }
